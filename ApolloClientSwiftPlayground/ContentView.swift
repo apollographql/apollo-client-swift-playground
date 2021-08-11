12a0bdbabba641551cc90642b1247c780d4bd350
Calvin Cestari <calvin.cestari@gmail.com>
2021-08-11 14:17:40 -0700

    Create mac app and copy files from apollo-ios

---
+++ ApolloClientSwiftPlayground/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  ApolloClientSwiftPlayground
+//
+//  Created by Calvin Cestari on 2021-08-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
